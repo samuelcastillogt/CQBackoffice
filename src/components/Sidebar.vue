3e5f5dbe3a54829c3eff456ccd3712c4c77c2568
Samuel Castillo <samuelcastillo@Mac-mini-de-Samuel.local>
2025-12-16 18:33:55 -0600

    layout basico

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,16 @@
+<template>
+    <section class="sidebar">
+        <div class="sidebar-option">
+            <p>Inicio</p>
+        </div>
+    </section>
+</template>
+
+<style scoped>
+    .sidebar{
+        background-color: #436850;
+        width: 200px;
+        height: 100vh;
+        color: #f8fafc;
+    }
+</style>
